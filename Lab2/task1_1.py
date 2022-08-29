9ee43df68767c9dc783fa96ecc384cc2fa4dcc73
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-08-29 10:27:49 +0300

    refactoring of lab/task1_1

--- Lab2/task1_1.py
+++ Lab2/task1_1.py
@@ -1,26 +1,45 @@
 import math
 
 
-while True:
-    try:
-        x = float(input('Enter number X: '))
-        y = float(input('Enter number Y: '))
-    except ValueError as ve:
-        print(f"An error {ve}. Please, try again")
-    else:
-        if (2 * x) > 709:
-            print(f"Enter a smaller number x: ")
-            continue
+# solve this function without any errors
+def calculate_func(x, y):
+    result = (math.exp(2 * x) + math.sin(y)) / (math.log(3.8 * x + y))
+    return result
+
+
+def get_numbers():
+    while True:
+        try:
+            x = float(input('Enter number X: '))
+            y = float(input('Enter number Y: '))
+        except ValueError as ve:
+            print(f"An error - {ve}. Please, try again")
         else:
-            numerator = math.exp(2 * x) + math.sin(y)
-            number = 3.8 * x + y
-            if number > 0:
-                denominator = math.log(number)
-                if denominator < 0:
-                    print("Не можна ділити на нуль")
-                else:
-                    R = numerator / denominator
-                    print(f"Результат --> {R}")
-                    break
-            else:
-                print("В логарифмі число менше за нуль")
+            break
+    return x, y
+
+
+def save_calculate(func):
+    def save_version(x, y):
+        if (2 * x) > 709:
+            print(f"Enter a smaller number x")
+            return
+        elif 3.8 * x + y < 0:
+            print("There's the number less than zero in the logarithm")
+            return
+        elif math.log(3.8 * x + y) < 0:
+            print("The denominator is less than zero")
+            return
+        return func(x, y)
+
+    return save_version
+
+
+def main():
+    calculate_safe = save_calculate(calculate_func)
+    x, y = get_numbers()
+    print(calculate_safe(x, y))
+
+
+if "__main__" == __name__:
+    main()
--- Lab2/task2_1.py
+++ Lab2/task2_1.py
@@ -1,32 +1,23 @@
 import math
 
-while True:
+
+def calculate(x, y):
     try:
-        x = float(input("Enter number 'x': "))
-        y = float(input("Enter number 'y': "))
+        x = float(x)
+        y = float(y)
+        denominator = math.cos(2 * y)
+        numerator = 3.5 * x + 1
+        r = math.log(x) + (numerator / denominator)
     except ValueError as ve:
         print(f"There is an error {ve}")
-    else:
-        denominator = math.cos(2 * y)
-        numerator = 3.5*x+1
-        if x > 0 and denominator != 0:
-            R = math.log(x) + (numerator/denominator)
-            print(f"Result is {R}")
-            break
-        print("X and denominator can't be equal to 0")
-
-while True:
-    try:
-        x = float(input("Enter number 'x': "))
-        y = float(input("Enter number 'y': "))
-        denominator = math.sin(2 * y)
-        numerator = 3.5 * x + 1
-        R = math.log(x) + (numerator / denominator)
-    except ValueError as ve:
-        print(f"There is an error: {ve}")
     except ZeroDivisionError as zd:
-        print(f"There is an error: {zd}")
+        print(f"There is an error {zd}")
+    except TypeError as te:
+        print(f"There is an error {te}")
     else:
-        print(f"Result is {R}")
-        break
+        print(f"Result is {r}")
 
+
+a = input("Enter number 'x': ")
+b = input("Enter number 'y': ")
+calculate(a, b)
